c9c3acedbb020f7b66fc121b2c5e59d11b88b083
migzzz1018 <99191520+migzzz1018@users.noreply.github.com>
2023-06-25 22:14:21 +0800

    added

--- SERVER/models/Post.js
+++ SERVER/models/Post.js
@@ -19,7 +19,7 @@ const postSchema = mongoose.Schema(
             of: Boolean,
         },
         comments: {
-            types: Array,
+            type: Array,
             default: []
         }
     },
