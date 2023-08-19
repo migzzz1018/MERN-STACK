268a6256cfac6791648239e204f9a2a61ab56829
migzzz1018 <99191520+migzzz1018@users.noreply.github.com>
2023-08-19 16:08:41 +0800

    Update

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,8 +1,22 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 
 const LoginPage = () => {
-    return (
-        <div>LoginPage</div>
-    )
-}
+  const theme = useTheme();
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          TwitterCopy
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
